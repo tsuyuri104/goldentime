06c5c84051d625bfee15b457a47af9cf35affa37
tsuyuri104 <tsuyuri104@gmail.com>
2022-01-03 11:23:24 +0900

    Monthlyコンポーネントにカレンダーを表示する処理を追加

--- src/app/components/monthly-data/monthly-data.component.ts
+++ src/app/components/monthly-data/monthly-data.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Calendar } from 'src/app/interfaces/calendar';
+import { CalendarDay } from 'src/app/interfaces/calendar-day';
+import { CalendarRow } from 'src/app/interfaces/calendar-row';
+import { DailyService } from 'src/app/services/daily.service';
+import { MonthlyService } from 'src/app/services/monthly.service';
+import { UrdayinService } from 'src/app/services/urdayin.service';
 
 @Component({
   selector: 'app-monthly-data',
@@ -7,9 +13,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonthlyDataComponent implements OnInit {
 
-  constructor() { }
+  //#region 変数
 
-  ngOnInit(): void {
+  public selectedMonth: Date = new Date();
+  public calendar: Calendar = { rows: [] };
+
+  //#endregion
+
+  //#region コンストラクタ
+  constructor(private sMonthly: MonthlyService, private sDaily: DailyService, private sUrdayin: UrdayinService) {
+
   }
+  //#endregion
 
+  //#region イベント
+
+  //#region ngOnInit
+  /**
+   * 初期設定
+   */
+  ngOnInit(): void {
+    this.createCalender();
+  }
+  //#endregion
+
+  //#endregion
+
+  //#region メソッド
+
+  //#region setSelectedDate
+  /**
+   * 対象日を設定する
+   * @param date カレンダーの日にち
+   */
+  public setSelectedDate(date: number): void {
+
+  }
+  //#endregion
+
+  //#region createCalender
+  /**
+   * カレンダーを作成する
+   */
+  private createCalender() {
+    let calender: Calendar = { rows: [] }
+
+    //月の一日を取得する
+    const firstDate: Date = this.getFirstDate();
+
+    //月の最終日を取得する
+    const lastDate: Date = this.getLastDate();
+
+    //カレンダーの１行分の情報を格納する
+    let row: CalendarRow = { days: [] };
+
+    //カレンダーの１行目の先月分の箇所に空白を設定する
+    const firstDayOfWeek: number = firstDate.getDay();
+    for (let i = 0; i < firstDayOfWeek; i++) {
+      row.days.push(<CalendarDay>{
+        date: 0,
+        isSunday: false,
+        isSaturday: false,
+        isBlank: true,
+      });
+    }
+
+    //カレンダーの日付分を設定する
+    const daysInMonth: number = lastDate.getDate();
+    for (let i = 0; i < daysInMonth; i++) {
+
+      //曜日を取得する
+      const tmpDate: Date = this.addDate(firstDate, i);
+      const dayOfWeek: number = tmpDate.getDay();
+
+      //日曜日の場合
+      if (dayOfWeek === 0) {
+        calender.rows.push(row);
+        row = { days: [] };
+      }
+
+      const isSunday: boolean = dayOfWeek === 0;
+      const isSaturday: boolean = dayOfWeek === 6;
+
+      row.days.push(<CalendarDay>{
+        date: tmpDate.getDate(),
+        isSunday: isSunday,
+        isSaturday: isSaturday,
+        isBlank: false
+      });
+    }
+
+    //カレンダーの最終行の来月分の箇所に空白を設定する
+    const lastDayOfWeek: number = lastDate.getDay();
+    for (let i = lastDayOfWeek; i < 7; i++) {
+      row.days.push(<CalendarDay>{
+        date: 0,
+        isSunday: false,
+        isSaturday: false,
+        isBlank: true,
+      });
+    }
+
+    calender.rows.push(row);
+
+    this.calendar = calender;
+  }
+  //#endregion
+
+  //#region addDate
+  /**
+   * 日付を加算する
+   * @param date 元になる日付
+   * @param addDays 加算日数
+   * @returns 加算した日付
+   */
+  private addDate(date: Date, addDays: number): Date {
+    var date = new Date(date.valueOf());
+    date.setDate(date.getDate() + addDays);
+    return date;
+  }
+  //#endregion
+
+  //#region getFirstDate
+  /**
+   * 月の初日を取得する
+   * @returns 月の初日
+   */
+  private getFirstDate(): Date {
+    let year: number = this.selectedMonth.getFullYear();
+    let month: number = this.selectedMonth.getMonth();
+
+    return new Date(year, month, 1);
+  }
+  //#endregion
+
+  //#region getLastDate
+  /**
+   * 月の最終日を取得する
+   * @returns 月の最終日
+   */
+  private getLastDate(): Date {
+    let year: number = this.selectedMonth.getFullYear();
+    let month: number = this.selectedMonth.getMonth() + 1;
+
+    return new Date(year, month, 0);
+  }
+  //#endregion
+
+  //#endregion
 }
