29bae5eeca8056ac5472341ef4ae14fe679a184f
tsuyuri104 <tsuyuri104@gmail.com>
2022-01-04 21:50:33 +0900

    コメントを追加

--- src/app/utilities/common.ts
+++ src/app/utilities/common.ts
@@ -2,9 +2,16 @@ import { DatePipe } from "@angular/common"
 
 export class Common {
 
+    //#region dateToStrig
+    /**
+     * 日付型を文字列に変換する
+     * @param date 
+     * @returns 文字列yyyyMMdd
+     */
     public static dateToStrig(date: Date): string {
         const datepipe: DatePipe = new DatePipe('en-US');
         let formatted: string | null = datepipe.transform(date, 'yyyyMMdd');
         return formatted == null ? "" : formatted;
     }
+    //#endregion
 }
