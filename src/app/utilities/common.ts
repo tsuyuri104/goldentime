45410ef9431d129323804ca83c20128cb00a6f1c
tsuyuri104 <tsuyuri104@gmail.com>
2022-01-04 22:08:30 +0900

    日付型から文字列型変換の処理を追加

--- src/app/utilities/common.ts
+++ src/app/utilities/common.ts
@@ -9,8 +9,31 @@ export class Common {
      * @returns 文字列yyyyMMdd
      */
     public static dateToString(date: Date): string {
+        return this.dateToStringFormat(date, 'yyyyMMdd');
+    }
+    //#endregion
+
+    //#region dateToStringYearMonth
+    /**
+     * 日付を文字列に変換する（年月用）
+     * @param date 
+     * @returns 文字列yyyyMM
+     */
+    public static dateToStringYearMonth(date: Date): string {
+        return this.dateToStringFormat(date, 'yyyyMM');
+    }
+    //#endregion
+
+    //#region dateToStringFormat
+    /**
+     * 日付を指定のフォーマットで文字列に変換する
+     * @param date 
+     * @param format 
+     * @returns 
+     */
+    private static dateToStringFormat(date: Date, format: string): string {
         const datepipe: DatePipe = new DatePipe('en-US');
-        let formatted: string | null = datepipe.transform(date, 'yyyyMMdd');
+        let formatted: string | null = datepipe.transform(date, format);
         return formatted == null ? "" : formatted;
     }
     //#endregion
