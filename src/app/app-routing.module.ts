bbb0059c2ac2e9bbffc19de714064c34f8644dd0
tsuyuri104 <tsuyuri104@gmail.com>
2022-01-29 12:01:25 +0900

    OverviewからRegisterに名前を変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteName } from './classes/route-name';
 import { LoginComponent } from './components/login/login.component';
-import { OverviewComponent } from './components/overview/overview.component';
+import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: RouteName.LOGIN, pathMatch: 'full' },
   { path: RouteName.LOGIN, component: LoginComponent },
-  { path: RouteName.OVERVIEW, component: OverviewComponent, canActivate: [AuthGuard] },
+  { path: RouteName.REGISTER, component: RegisterComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: RouteName.LOGIN }
 ];
 
