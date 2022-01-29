2fd66e5169c53dc4500868071b868b2e437223eb
tsuyuri104 <tsuyuri104@gmail.com>
2022-01-29 12:03:05 +0900

    ログアウトボタンの位置を変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RouteName } from './classes/route-name';
+import { AuthService } from './services/auth.service';
 import { VersionService } from './services/version.service';
 
 @Component({
@@ -7,10 +11,62 @@ import { VersionService } from './services/version.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  //#region 変数
+
   public title = 'urdayin';
   public version = '';
+  public isLogined: boolean = false;
 
-  constructor(private sVersion: VersionService) {
+  private subscriptionIsLogined!: Subscription;
+
+  //#endregion
+
+  //#region コンストラクタ
+  constructor(private sVersion: VersionService, private sAuth: AuthService, private router: Router) {
     this.version = this.sVersion.version;
   }
+  //#endregion
+
+  //#region イベント
+
+  //#region ngOnInit
+  /**
+   * 初期設定
+   */
+  ngOnInit(): void {
+    //監視対象の設定
+    this.subscriptionIsLogined = this.sAuth.sharedIsLoginedDataSource$.subscribe(
+      isLogined => {
+        this.isLogined = isLogined;
+      }
+    );
+  }
+  //#endregion
+
+  //#region ngOnDestroy
+  /**
+   * 破棄設定
+   */
+  ngOnDestroy(): void {
+    this.subscriptionIsLogined.unsubscribe();
+  }
+  //#endregion
+
+  //#endregion
+
+  //#region メソッド
+
+  //#region logoutProcess
+  /**
+   * ログアウト処理
+   */
+  public logoutProcess() {
+    this.sAuth.logout().then(() => {
+      this.router.navigateByUrl(RouteName.LOGIN);
+    });
+  }
+  //#endregion
+
+  //#endregion
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { signInWithEmailAndPassword, getAuth, UserCredential, signOut } from 'firebase/auth';
+import { Subject } from 'rxjs';
 import { UrdayinService } from './urdayin.service';
 
 @Injectable({
@@ -9,13 +10,27 @@ export class AuthService {
 
   //#region 変数
 
+  private sharedIsLoginedDataSource = new Subject<boolean>();
+
   public user: UserCredential | undefined;
+  public sharedIsLoginedDataSource$ = this.sharedIsLoginedDataSource.asObservable();
 
   //#endregion
 
   //#region コンストラクタ
   constructor(private sUrdayin: UrdayinService) {
 
+  }
+  //#endregion
+
+  //#region onSharedIsLoginedDateChanged
+  /**
+   * 監視対象
+   * ログインしているか判定
+   * @param isLogined 
+   */
+  public onSharedIsLoginedDateChanged(isLogined: boolean): void {
+    this.sharedIsLoginedDataSource.next(isLogined);
   }
   //#endregion
 
@@ -32,6 +47,7 @@ export class AuthService {
       .then((u) => {
         this.user = u;
         this.sUrdayin.setSelectedUser(u);
+        this.onSharedIsLoginedDateChanged(true);
       });
   }
   //#endregion
@@ -46,6 +62,7 @@ export class AuthService {
     return signOut(auth)
       .then(() => {
         this.user = undefined;
+        this.onSharedIsLoginedDateChanged(false);
       });
   }
   //#endregion
