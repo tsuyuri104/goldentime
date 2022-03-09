11180ff8a79e0c21039596d2d428ec11b570cd3b
tsuyuri104 <tsuyuri104@gmail.com>
2022-03-09 14:17:00 +0900

    Appコンポーネントを修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,41 +12,14 @@ export class AppComponent {
   //#region 変数
 
   public title = 'urdayin';
-  public isContentPage: boolean = true;
-
-  private subscriptionIsContentPage!: Subscription;
 
   //#endregion
 
   //#region コンストラクタ
-  constructor(private sComponetCotrol: ComponentControlService) {
+  constructor() {
 
   }
   //#endregion
 
-  //#region イベント
 
-  //#region ngOnInit
-  /**
-   * 初期設定
-   */
-  ngOnInit(): void {
-    this.subscriptionIsContentPage = this.sComponetCotrol.sharedIsContentPage$.subscribe(
-      isContentPage => {
-        this.isContentPage = isContentPage;
-      }
-    )
-  }
-  //#endregion
-
-  //#region ngOnDestroy
-  /**
-   * 破棄設定
-   */
-  ngOnDestroy(): void {
-    this.subscriptionIsContentPage.unsubscribe();
-  }
-  //#endregion
-
-  //#endregion
 }
