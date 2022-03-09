e57602685a8a4910f17be16f22aa4dbb5ee9ab39
tsuyuri104 <tsuyuri104@gmail.com>
2022-03-09 14:17:29 +0900

    ルーティングを修正（サブルーティングを作成）

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteName } from './classes/route-name';
-import { LoginComponent } from './components/login/login.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { RegisterComponent } from './components/register/register.component';
+import { FullscreenDefaultComponent } from './components/fullscreenpage/fullscreen-default/fullscreen-default.component';
+import { LoginComponent } from './components/fullscreenpage/login/login.component';
+import { OverviewComponent } from './components/splitpage/overview/overview.component';
+import { RegisterComponent } from './components/splitpage/register/register.component';
+import { SplitDefaultComponent } from './components/splitpage/split-default/split-default.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: RouteName.LOGIN, pathMatch: 'full' },
-  { path: RouteName.LOGIN, component: LoginComponent },
-  { path: RouteName.REGISTER, component: RegisterComponent, canActivate: [AuthGuard] },
-  { path: RouteName.OVERVIEW, component: OverviewComponent, canActivate: [AuthGuard] },
+  { path: RouteName.LOGIN, component: FullscreenDefaultComponent, children: [{ path: '', component: LoginComponent }] },
+  {
+    path: '', component: SplitDefaultComponent, children: [
+      { path: RouteName.REGISTER, component: RegisterComponent },
+      { path: RouteName.OVERVIEW, component: OverviewComponent }]
+    , canActivate: [AuthGuard]
+  },
   { path: '**', redirectTo: RouteName.LOGIN }
 ];
 
