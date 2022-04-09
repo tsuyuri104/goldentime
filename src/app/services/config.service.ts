bafebfcfe5aed95365eb274d8952fba7290bda47
tsuyuri104 <tsuyuri104@gmail.com>
2022-04-09 17:43:51 +0900

    バージョンアップ

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -8,7 +8,7 @@ export class ConfigService {
   //#region 変数
 
   public readonly appName: string = "urdayin";
-  public readonly version: string = "2.2.6";
+  public readonly version: string = "2.3.0";
 
   public readonly registerStartYear: number = 2022;
 
