a1454f8c0533b5fbac00de54e00b9b8dd5ecd394
tsuyuri104 <tsuyuri104@gmail.com>
2022-04-30 09:29:53 +0900

    コメントインターフェースの型を修正

--- src/app/interfaces/document/comment.ts
+++ src/app/interfaces/document/comment.ts
@@ -1,7 +1,7 @@
 import { Timestamp } from "firebase/firestore";
 
 export interface Comment {
-    comment: string[],
-    commenter: string[],
+    comment: string,
+    commenter: string,
     create: Timestamp,
 }
