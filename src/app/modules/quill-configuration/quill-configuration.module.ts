26e4c4db871ce37a282a12010adbc1a5b19380a7
tsuyuri104 <tsuyuri104@gmail.com>
2022-05-01 14:48:34 +0900

    入力種類を修正

--- src/app/modules/quill-configuration/quill-configuration.module.ts
+++ src/app/modules/quill-configuration/quill-configuration.module.ts
@@ -2,7 +2,8 @@ import { QuillModules } from "ngx-quill";
 
 export const QuillConfiguration: QuillModules = {
   toolbar: [
-    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    // ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['bold', 'underline', 'strike'],
     ['blockquote', 'code-block'],
 
     [{ 'header': 1 }, { 'header': 2 }],               // custom button values
@@ -11,12 +12,11 @@ export const QuillConfiguration: QuillModules = {
     [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
     // [{ 'direction': 'rtl' }],                         // text direction
 
-    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
     // [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
     // [{ 'font': [] }],
-    [{ 'color': [] }],          // dropdown with defaults from theme
-    [{ 'align': [] }],
+    // [{ 'align': [] }],
   ],
 }
