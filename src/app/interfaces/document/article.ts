d7aac742fe853a29688501a05258df8d924068b4
tsuyuri104 <tsuyuri104@gmail.com>
2022-05-01 21:22:11 +0900

    文字列からType型に変更

--- src/app/interfaces/document/article.ts
+++ src/app/interfaces/document/article.ts
@@ -1,11 +1,12 @@
 import { Timestamp } from "firebase/firestore";
+import { ArticleStatus } from "src/app/types/article-status";
 import { Reaction } from "./reaction";
 
 export interface Article {
     writer: string,
     create_timestamp: Timestamp,
     update_timestamp: Timestamp,
-    status: string,
+    status: ArticleStatus,
     last_edition: number,
     reactions: Reaction,
     summary_title: string,
