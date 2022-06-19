c27887399f7156353110add2bd94026c28939fde
tsuyuri104 <tsuyuri104@gmail.com>
2022-06-19 15:01:39 +0900

    編集履歴の比較を任意のバージョンで比較できるように修正

--- src/app/components/splitpage/report-differ/report-differ.component.ts
+++ src/app/components/splitpage/report-differ/report-differ.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Timestamp } from 'firebase/firestore';
 import { ArticleData } from 'src/app/interfaces/component/article-data';
 import { ExEdition } from 'src/app/interfaces/component/ex-edition';
+import { InputOfFrmVersion } from 'src/app/interfaces/component/input-of-frm-version';
 import { ArticleService } from 'src/app/services/article.service';
 import { EditionsService } from 'src/app/services/editions.service';
 import { UrdayinService } from 'src/app/services/urdayin.service';
@@ -18,7 +20,11 @@ export class ReportDifferComponent implements OnInit {
   //#region 変数
 
   public after: ExEdition = this.getEmptyExEdition();
+  public afterVersion: number = 0;
   public before: ExEdition = this.getEmptyExEdition();
+  public beforeVersion: number = 0;
+  public versionOptions: number[] = [];
+  public frmVersion!: FormGroup;
 
   //#endregion
 
@@ -26,8 +32,12 @@ export class ReportDifferComponent implements OnInit {
   constructor(private route: ActivatedRoute,
     private sEditions: EditionsService,
     private sArticle: ArticleService,
-    private sUrdayin: UrdayinService) {
-
+    private sUrdayin: UrdayinService,
+    private fb: FormBuilder) {
+    //検索条件の初期化
+    this.frmVersion = this.fb.group({
+      version: 0,
+    });
   }
   //#endregion
 
@@ -38,7 +48,7 @@ export class ReportDifferComponent implements OnInit {
    * 初期設定
    */
   public async ngOnInit(): Promise<void> {
-    await this.getArticle();
+    this.setInit();
   }
   //#endregion
 
@@ -46,18 +56,62 @@ export class ReportDifferComponent implements OnInit {
 
   //#region メソッド
 
+  //#region setText
+  /**
+   * テキストをエレメントに設定する
+   */
+  public setText(): void {
+    const condition: InputOfFrmVersion = this.frmVersion.value;
+    const id: string = this.getArticleId();
+
+    this.getArticle(id, this.afterVersion, Number(condition.version));
+  }
+  //#endregion
+
+  //#region setInit
+  /**
+   * 初期設定
+   */
+  private async setInit(): Promise<void> {
+    const id: string = this.getArticleId();
+    const article: ArticleData = await this.sArticle.getArticleData(id, this.sUrdayin.getSelectedUser());
+    const lastEdition: number = article.article.last_edition;
+
+    this.getArticle(id, lastEdition, lastEdition - 1);
+    this.setVersionOptions(lastEdition);
+
+    this.afterVersion = lastEdition;
+
+    this.frmVersion = this.fb.group({
+      version: lastEdition - 1,
+    });
+  }
+  //#endregion
+
+  //#region setVersionOptions
+  /**
+   * バージョンの選択肢を設定する
+   * @param lastEdition 
+   */
+  private setVersionOptions(lastEdition: number): void {
+    let ops: number[] = [];
+
+    for (let i = lastEdition - 1; i > 0; i--) {
+      ops.push(i);
+    }
+
+    this.versionOptions = ops;
+  }
+  //#endregion
+
   //#region getArticle
   /**
    * 記事データを取得する
    */
-  private async getArticle(): Promise<void> {
+  private async getArticle(id: string, afterVersion: number, beforeVersion: number): Promise<void> {
 
-    const id: string = this.getArticleId();
-
-    const article: ArticleData = await this.sArticle.getArticleData(id, this.sUrdayin.getSelectedUser());
-
-    let after: ExEdition = await this.sEditions.getEdition(id, article.article.last_edition);
-    let before: ExEdition = await this.sEditions.getEdition(id, (article.article.last_edition - 1));
+    let after: ExEdition = await this.sEditions.getEdition(id, afterVersion);
+    let before: ExEdition = await this.sEditions.getEdition(id, beforeVersion);
 
     after.text = this.convertKaigyo(after.text);
     before.text = this.convertKaigyo(before.text);
