71983f27ab2a406369be07c717435a39c52b7fdd
tsuyuri104 <tsuyuri104@gmail.com>
2022-08-08 21:08:46 +0900

    POSTデータの値検証処理を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -51,3 +51,133 @@ export const SetDailyData = functions.https.onCall(async (data, context) => {
     }
 });
 
+//#region isValidData
+/**
+ * 値の検証
+ * @param data 
+ * @returns 
+ */
+function isValidData(data: any): string {
+
+    let errorMessage: string = "";
+
+    if (data.email === undefined) {
+        errorMessage += "emailがありません。";
+    }
+
+    if (data.key === undefined) {
+        errorMessage += "keyがありません。";
+    }
+
+    if (data.kosu === undefined) {
+        errorMessage += "kosuがありません。";
+    }
+
+    // 存在しない場合はここまで
+    if (errorMessage !== "") {
+        return errorMessage;
+    }
+
+    if (data.kosu.length === 0) {
+        errorMessage += "kosuがありません。";
+    }
+
+    data.kosu.forEach((datum: any) => {
+        if (datum.date === undefined) {
+            errorMessage += "dateがありません。";
+        }
+
+        if (datum.task === undefined) {
+            errorMessage += "taskがありません。";
+        }
+
+        if (datum.hours === undefined) {
+            errorMessage += "hoursがありません。";
+        }
+
+        // 存在しない場合はここまで
+        if (errorMessage !== "") {
+            return;
+        }
+
+        // 日付チェック
+        if (!isValidDate(datum.date)) {
+            errorMessage += "dateの値が不正です。";
+        }
+
+        // 数値チェック
+        if (isNaN(Number(datum.hours))) {
+            errorMessage += "hoursの値が不正です。";
+        }
+
+    });
+
+    return errorMessage;
+}
+//#endregion
+
+//#region isValidDate
+/**
+ * 日付の検証
+ * @param value 
+ * @returns 
+ */
+function isValidDate(value: string): boolean {
+
+    // 桁数チェック
+    if (value.length !== 8) {
+        return false;
+    }
+
+    // 数値チェック
+    const y: number = Number(value.substring(0, 4));
+    const m: number = Number(value.substring(4, 6));
+    const d: number = Number(value.substring(6, 8));
+
+    if (isNaN(y)) {
+        return false;
+    }
+
+    if (isNaN(m)) {
+        return false;
+    }
+
+    if (isNaN(d)) {
+        return false;
+    }
+
+    // 日付型チェック（変換後の数字が変換前の数値と同じかチェック）
+    const date: Date = new Date(y, m - 1, d);
+
+    if (date.getFullYear() !== y) {
+        console.log("年不正", date.getFullYear(), y);
+        return false;
+    }
+
+    if (date.getMonth() + 1 !== m) {
+        console.log("月不正", date.getMonth(), m);
+        return false;
+    }
+
+    if (date.getDate() !== d) {
+        console.log("日不正", date.getDate(), d);
+        return false;
+    }
+
+    return true;
+}
+//#endregion
+
+//#region isValidUserKey
+/**
+ * ユーザーとキーの検証
+ * @param email 
+ * @param key 
+ * @returns 
+ */
+async function isValidUserKey(email: string, key: string): Promise<boolean> {
+    const db = admin.firestore();
+    const docs = await db.collection("apikey").where("email", "==", email).where("key", "==", key).get();
+    return !docs.empty;
+}
+//#endregion
