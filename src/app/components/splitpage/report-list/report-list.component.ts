7a8fadcc202a35f92305b1916f5b97a0790aeb60
tsuyuri104 <tsuyuri104@gmail.com>
2022-08-21 15:52:56 +0900

    記事ダウンロード処理を実装

--- src/app/components/splitpage/report-list/report-list.component.ts
+++ src/app/components/splitpage/report-list/report-list.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouteName } from 'src/app/classes/route-name';
+import { ArticleData } from 'src/app/interfaces/component/article-data';
 import { ArticleList } from 'src/app/interfaces/component/article-list';
+import { Reactioner } from 'src/app/interfaces/document/reactioner';
 import { ArticleService } from 'src/app/services/article.service';
+import { FileService } from 'src/app/services/file.service';
+import { ReactionerService } from 'src/app/services/reactioner.service';
 import { UrdayinService } from 'src/app/services/urdayin.service';
 import { ArticleStatus } from 'src/app/types/article-status';
+import { DateUtil } from 'src/app/utilities/date-util';
 
 @Component({
   selector: 'app-report-list',
@@ -20,7 +25,11 @@ export class ReportListComponent implements OnInit {
   //#endregion
 
   //#region コンストラクタ
-  constructor(private sArticle: ArticleService, private sUrdayin: UrdayinService, private router: Router) {
+  constructor(private sArticle: ArticleService
+    , private sUrdayin: UrdayinService
+    , private router: Router
+    , private sFile: FileService
+    , private sReactioner: ReactionerService) {
 
   }
   //#endregion
@@ -47,6 +56,19 @@ export class ReportListComponent implements OnInit {
    */
   public transitionViewer(id: string): void {
     this.router.navigateByUrl("/" + RouteName.VIEWER + "/" + id);
+  }
+  //#endregion
+
+  //#region exportArticle
+  /**
+   * 記事ファイルを出力する
+   * @param id 
+   * @param title 
+   */
+  public async exportArticle(id: string, title: string): Promise<void> {
+    const dom: Document = await this.createFile(id);
+    const stringDom: string = String(dom.documentElement.outerHTML);
+    this.sFile.download(stringDom, title, "text/html");
   }
   //#endregion
 
@@ -80,6 +102,80 @@ export class ReportListComponent implements OnInit {
       .then((data) => {
         this.articleList = data;
       });
+  }
+  //#endregion
+
+  //#region createFile
+  /**
+   * 記事ファイルを作成する
+   * @param id 
+   * @returns 
+   */
+  private async createFile(id: string): Promise<Document> {
+
+    const email: string = this.sUrdayin.getSelectedUser();
+
+    //記事データを取得する
+    const articleData: ArticleData = await this.sArticle.getArticleData(id, email);
+
+    //閲覧者の反応データを取得する
+    const reactioners: Reactioner[] = await this.sReactioner.getReactionerData(id, email);
+
+    //自分の記事か判定する
+    articleData.isMine = articleData.article.writer === email;
+
+    // テンプレートファイルを取得する
+    const text = await (await fetch("assets/article.html")).text();
+    let dom = new DOMParser().parseFromString(text, "text/html");
+
+    // データをテンプレートに埋め込む
+    dom.head.getElementsByTagName("title")[0].innerHTML = articleData.text.title;
+
+    dom.getElementById("articleTitle")!.innerHTML = articleData.text.title;
+    dom.getElementById("articleWriter")!.innerHTML = articleData.article.writer_name;
+    dom.getElementById("articleCreateDateTime")!.innerHTML = DateUtil.toStringDateTime(articleData.text.create_timestamp.toDate());
+    dom.getElementById("articleText")!.innerHTML = articleData.text.text;
+
+    dom.getElementById("reactionHeart")!.innerHTML = String(articleData.article.reactions.heart);
+    dom.getElementById("reactionClap")!.innerHTML = String(articleData.article.reactions.clap);
+    dom.getElementById("reactionThumbsup")!.innerHTML = String(articleData.article.reactions.thumbsup);
+
+    const template: HTMLElement = <HTMLElement>dom.getElementsByClassName("comment-template")[0];
+    let wrapper: HTMLElement = dom.getElementById("commentWrapper")!;
+    articleData.comments.forEach(comment => {
+      let divComment: HTMLElement = this.copyTemplateElement(template);
+
+      divComment.classList.remove("comment-template");
+
+      divComment.getElementsByClassName("comment-commenter")[0].innerHTML = comment.commenter_name;
+      divComment.getElementsByClassName("comment-card-body")[0].innerHTML = comment.comment;
+      divComment.getElementsByClassName("comment-date")[0].innerHTML = DateUtil.toStringDateTime(comment.create_timestamp.toDate());
+
+      wrapper.appendChild(divComment);
+    });
+
+    return dom;
+  }
+  //#endregion
+
+  //#region copyTemplateElement
+  /**
+   * テンプレートエレメントを複製する
+   * @param elm 
+   * @returns 
+   */
+  private copyTemplateElement(elm: HTMLElement): HTMLElement {
+    let ret: HTMLElement = document.createElement(elm.tagName);
+
+    elm.classList.forEach(item => {
+      ret.classList.add(item);
+    });
+
+    for (let i = 0; i < elm.children.length; i++) {
+      ret.appendChild(this.copyTemplateElement(<HTMLElement>elm.children[i]));
+    }
+
+    return ret;
   }
   //#endregion
 
