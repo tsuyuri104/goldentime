2306ca770a2014c9277a64dcc2d19308d337b306
tsuyuri104 <tsuyuri104@gmail.com>
2023-02-11 21:14:24 +0900

    Revert "分析画面の構成を分割"
    This reverts commit 404d43dfc06b8102575f014ec8d15a211de501fc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,8 +48,6 @@ import { SvgLockComponent } from './components/svg/svg-lock/svg-lock.component';
 import { SvgUnlockComponent } from './components/svg/svg-unlock/svg-unlock.component';
 import { AnalysisComponent } from './components/splitpage/analysis/analysis.component';
 import { SvgAnalysisComponent } from './components/svg/svg-analysis/svg-analysis';
-import { SummaryComponent } from './components/splitpage/analysis/summary/summary.component';
-import { DetailComponent } from './components/splitpage/analysis/detail/detail.component';
 
 registerLocaleData(localeJa);
 
@@ -84,8 +82,6 @@ registerLocaleData(localeJa);
     SvgUnlockComponent,
     AnalysisComponent,
     SvgAnalysisComponent,
-    SummaryComponent,
-    DetailComponent,
   ],
   imports: [
     BrowserModule,
